6152a0f49aee3ea468322607feafd4ddc4759ab2
DYLAN MAXIMILIANO GUERRERO GONZALEZ <dyguerrero@estudiantec.cr>
2025-09-24 21:38:31 -0600

    Demo

--- untitled2/cmake-build-debug/CMakeFiles/clean_additional.cmake
+++ untitled2/cmake-build-debug/CMakeFiles/clean_additional.cmake
@@ -3,10 +3,13 @@ cmake_minimum_required(VERSION 3.16)
 
 if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
   file(REMOVE_RECURSE
+  "CMakeFiles\\demo_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\demo_autogen.dir\\ParseCache.txt"
   "CMakeFiles\\profiler_autogen.dir\\AutogenUsed.txt"
   "CMakeFiles\\profiler_autogen.dir\\ParseCache.txt"
   "CMakeFiles\\untitled2_autogen.dir\\AutogenUsed.txt"
   "CMakeFiles\\untitled2_autogen.dir\\ParseCache.txt"
+  "demo_autogen"
   "profiler_autogen"
   "untitled2_autogen"
   )
